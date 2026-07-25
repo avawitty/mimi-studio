a5a45039ef9193e3a4c678acc04fda46eee2694f
avawitty <avawittkop@gmail.com>
2026-07-25 05:21:48 -0700

    scaffold: Mimi inference layer - system prompt, API wrapper, configuration

---
+++ src/mimi/index.ts
@@ -0,0 +1,6 @@
+import Mimi from "./mimi";
+import mimiRouter from "./mimi-api";
+import { loadConfig, MimiConfig } from "./config";
+
+export { Mimi, mimiRouter, loadConfig };
+export type { MimiConfig };
